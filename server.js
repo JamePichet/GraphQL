fdc2c783ab4a8a54dec9966029be62de80567317
JamePichet <pichet.plu@gmail.com>
2018-01-18 16:29:31 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var app = express();
+var PORT = process.env.port || 3000
+var MyGraphQLSchema = require('./graphql/schema');
+var cors = require('cors');
+app.use(cors())
+app.use('/graphql', graphqlHTTP({
+  schema: MyGraphQLSchema,
+  graphiql: true
+}));
+app.listen(PORT);
+console.log("Server running on localhost:", PORT);
